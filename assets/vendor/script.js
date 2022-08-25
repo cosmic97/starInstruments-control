40ef78f6e45c605d3286b05abce391c03b019c4a
dhanrajs <dhanrajs@axioned.com>
2022-08-25 18:32:27 +0530

    updated images n made slick responsive

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
 	var hamburger = $('.hamburger');
   var navLinks = $('.nav-links');
 
+	//slick slider
+	$('.slideshow .slick').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	});
+
 	//hamburger menu to show navigation links on click
-  hamburger.click(function() {
-    hamburger.toggleClass('active');
-    navLinks.toggleClass('active');
-    $('body').toggleClass('hide-scroll');
-  });
+	hamburger.click(function() {
+		hamburger.toggleClass('active');
+		navLinks.toggleClass('active');
+		$('body').toggleClass('hide-scroll');
+	});
 });
